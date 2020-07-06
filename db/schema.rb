38914eb96927a5468c46f77f9d23187fc9d74c21
Kaycheng <kaycheng717@gmail.com>
2020-07-06 12:41:09 +0800

    Add avatar on users table and create application.yml.example

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_05_161352) do
+ActiveRecord::Schema.define(version: 2020_07_06_043353) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2020_07_05_161352) do
     t.string "username"
     t.string "provider"
     t.string "uid"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
