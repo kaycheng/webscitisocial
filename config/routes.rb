abb471ea570cd39ee4c2203c74a14f79b844ee73
Kaycheng <kaycheng717@gmail.com>
2020-07-10 16:20:26 +0800

    Create api/v1/subscribe action and use Rails ujs function to catch data

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,11 @@ Rails.application.routes.draw do
     resources :products, except: [:show]
     resources :vendors, except: [:show]
   end
+
+  # api/v1/subscribe
+  namespace :api do
+    namespace :v1 do
+      post :subscribe, to: "utils#subscribe"
+    end
+  end
 end
