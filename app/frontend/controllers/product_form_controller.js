50bdfed8f439ab310374250c006e2bbdc9ecc754
Kaycheng <kaycheng717@gmail.com>
2020-07-10 13:55:00 +0800

    Add _destroy action on sku form

--- app/frontend/controllers/product_form_controller.js
+++ app/frontend/controllers/product_form_controller.js
@@ -8,4 +8,16 @@ export default class extends Controller {
     let content = this.templateTarget.innerHTML.replace(/NEW_RECORD/g, new Date().getTime());
     this.linkTarget.insertAdjacentHTML('beforebegin', content);
   }
+
+  removeSku(event) {
+    event.preventDefault()
+
+    let wrapper = event.target.closest('.nested-fields');
+    if (wrapper.dataset.newRecord == "true") {
+      wrapper.remove();
+    } else {
+      wrapper.querySelector("input[name*='_destroy']").value = 1;
+      wrapper.style.display = 'none';
+    }
+  }
 }
