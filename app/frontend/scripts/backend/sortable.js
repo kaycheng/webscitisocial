7fe174fa238256f84cc1cf5ca0b4905aa91bf8ae
Kaycheng <kaycheng717@gmail.com>
2020-07-14 15:49:28 +0800

    Add sortablejs to change category's position

---
+++ app/frontend/scripts/backend/sortable.js
@@ -0,0 +1,14 @@
+import Sortable from 'sortablejs';
+
+document.addEventListener('turbolinks:load', () => {
+  let el = document.querySelector('.sortable-items');
+
+  if (el) {
+    Sortable.create(el, {
+      onEnd: event => {
+        let [model, id] = event.item.dataset.item.split('_');
+        console.log(model, id);
+      }
+    })
+  }
+})
