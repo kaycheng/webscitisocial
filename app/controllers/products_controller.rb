7cc52bb22df9e7bba55528b963bd8cd858719b7d
Kaycheng <kaycheng717@gmail.com>
2020-07-16 22:06:24 +0800

    Add content on product show page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,8 @@ class ProductsController < ApplicationController
   def index
     @products = Product.order(updated_at: :desc).includes(:vendor).with_attached_cover_image.with_rich_text_description_and_embeds
   end
+
+  def show
+    @product = Product.friendly.find(params[:id])
+  end
 end
