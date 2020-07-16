ea7d98172e83c7566bf8545f3c4197ea0262efd4
Kaycheng <kaycheng717@gmail.com>
2020-07-16 11:49:37 +0800

    Add with_cover_image and with_rich_text_description_and_embeds to avoid N+1

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.order(updated_at: :desc).includes(:vendor).with_attached_cover_image.with_rich_text_description_and_embeds
   end
 end
