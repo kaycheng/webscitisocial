87b8344173185fad9dfafb70ebf0d54830be6230
Kaycheng <kaycheng717@gmail.com>
2020-07-26 15:04:04 +0800

    Add cart button on navbar and use helper_method :current_cart to view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   before_action :find_category, unless: :backend?
+  helper_method :current_cart
 
   private
   def record_not_found
@@ -15,5 +16,9 @@ class ApplicationController < ActionController::Base
 
   def find_category
     @categories = Category.order(position: :asc)
+  end
+
+  def current_cart
+    @cart9527 ||= Cart.from_hash(session[:cart_9527])
   end
 end
