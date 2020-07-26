566bfa0af715d4e7e6554cd2dc32c08d2933a2c2
Kaycheng <kaycheng717@gmail.com>
2020-07-26 13:09:04 +0800

    Create cart_9527 session to store cart information

--- app/frontend/controllers/product_controller.js
+++ app/frontend/controllers/product_controller.js
@@ -1,7 +1,39 @@
 import { Controller } from "stimulus"
+import Rails from "@rails/ujs";
 
 export default class extends Controller {
-  static targets = [ "quantity" ]
+  static targets = [ "quantity", "sku", "addToCartButton" ]
+
+  addToCart(evt) {
+    evt.preventDefault();
+    
+    let product_id = this.data.get("id");
+    let quantity = this.quantityTarget.value;
+    let sku = this.skuTarget.value;
+
+    if (quantity > 0) {
+      this.addToCartButtonTarget.classList.add('spinner-grow', 'text-white')
+      this.addToCartButtonTarget.innerHTML = '';
+
+      let data = new FormData();
+      data.append("id", product_id);
+      data.append("quantity", quantity);
+      data.append("sku", sku);
+
+      Rails.ajax({
+        url: "/api/v1/cart",
+        data,
+        type: "POST",
+        success: resp => {
+          console.log(resp);
+        },
+        error: err => {
+          console.log(err);
+        }
+      });
+    }
+    
+  }
   
   quantityMinus(evt) {
     evt.preventDefault();
@@ -17,8 +49,5 @@ export default class extends Controller {
     this.quantityTarget.value = q + 1;
   }
 
-  o(evt) {
-    evt.preventDefault();
-    console.log('hola');
-  }
+  
 }
